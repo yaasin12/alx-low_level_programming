ee321e372025de158d79fa0c7b90fd863f60b14b
yaasin12 <yassin.j@live.com>
2022-09-08 14:42:04 +0000

    Task 4 try 3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/**
+ * Main program block
+ */
 
-int main(void){
-	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return 0;
 }
