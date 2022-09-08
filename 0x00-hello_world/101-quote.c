4da89e50b0e30a533a96202f53b21330a578394f
yaasin12 <yassin.j@live.com>
2022-09-08 08:48:00 -0700

    Task 8 try 2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - Program body
  *
  * Description - Write a C program that prints exactly and that piece of art is useful
  * " - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
  *
- * Return: 0
+ * Return: 1
  */
 int main(void)
 {
-	char xyz = 'and that piece of art is useful" - Dora Korpar, 2015-10-19';
-	putchar(xyz);
-	return (0);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
