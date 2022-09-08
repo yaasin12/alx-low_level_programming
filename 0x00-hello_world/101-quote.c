f36725ee57acdd54bcb6a0262e43f50b3bae14d8
yaasin12 <yassin.j@live.com>
2022-09-08 08:50:18 -0700

    Fix betty documentation syntax

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,9 +3,6 @@
 /**
  * main - Program body
  *
- * Description - Write a C program that prints exactly and that piece of art is useful
- * " - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
- *
  * Return: 1
  */
 int main(void)
