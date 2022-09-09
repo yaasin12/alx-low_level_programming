a206cec683b63f53a67974dc378c771abfdb4d77
yaasin12 <yassin.j@live.com>
2022-09-09 18:37:55 +0400

    Task 3 added

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * Main : main block
+ * return : 0 (success)
+ */
+
+int main(void)
+{
+    char lowercase = 'a';
+    char uppercase = 'A';
+
+    /*Print a to z*/
+    while (lowercase <= 'z')
+    {
+        putchar(lowercase);
+        ++lowercase;
+    }
+    /*Print A to Z*/
+    while (uppercase <= 'Z')
+    {
+        putchar(uppercase);
+        ++uppercase;
+    }
+    putchar(\n);
+
+    return (0);
+}
