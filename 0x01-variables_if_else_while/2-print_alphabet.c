35c35c72aaf24f18d9d641a7144e9684a5045bbd
yaasin12 <yassin.j@live.com>
2022-09-09 18:30:05 +0400

    Task 2 added

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char alp = 'a';
+
+	while (alp <= 'z')
+	{
+		putchar(alp);
+		ch++;
+	}
+	putchar('\n');
+
+	return (0);
+}
