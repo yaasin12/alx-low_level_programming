d6cfa45c255a5a000043264bf597ed13068724e0
yaasin12 <yassin.j@live.com>
2022-09-09 20:34:20 +0400

    Task 8 added

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ * Description: Prints base 16 numbers
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char y;
+	char x = '0';
+
+	while (x < '10')
+	{
+		putchar(d);
+		d++;
+	}
+	for (y = 'a'; y <= 'f'; y++)
+	{
+		putchar(y);
+	}
+	putchar('\n');
+	return (0);
+}
