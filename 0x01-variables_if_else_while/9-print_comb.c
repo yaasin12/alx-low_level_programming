969b65dc11705a2b175c8c3a1db4c712a761472e
yaasin12 <yassin.j@live.com>
2022-09-09 20:42:38 +0400

    Task 9 added

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ * Description: prints numbers 0-9 in ascending order
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a = '0'; a <= '9' ; a++)
+	{
+		putchar(a);
+
+		if (a != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
