8092a59794c141830429c4af2d59af5fff26ca11
yaasin12 <yassin.j@live.com>
2022-09-09 18:11:24 +0400

    remove last new line

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ int main(void)
 	else if (n2 == 0)
 		printf("Last digit of %i is %i and is 0\n", n, n2);
 	else if (n2 < 6)
-		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, n2);
+		printf("Last digit of %i is %i and is less than 6 and not 0", n, n2);
 
 	return (0);
 }
