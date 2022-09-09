3ae5de15ec373342c66774a676317055dcd03ea8
yaasin12 <yassin.j@live.com>
2022-09-09 18:44:29 +0400

    fix 2

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,25 +2,29 @@
 
 /**
  * Main : main block
+ * Description: print alphabet in lowercase and then in uppercase
  * return : 0 (success)
  */
+
 int main(void)
 {
-    char lowercase = 'a';
-    char uppercase = 'A';
-    /*Print a to z*/
-    while (lowercase <= 'z')
-    {
-        putchar(lowercase);
-        ++lowercase;
-    }
-    /*Print A to Z*/
-    while (uppercase <= 'Z')
-    {
-        putchar(uppercase);
-        ++uppercase;
-    }
-    putchar('\n');
+	char alp = 'a';
+	char ALP = 'A';
 
-    return (0);
+	/*prints a - z*/
+	while (alp <= 'z')
+	{
+		putchar(alp);
+		++alp;
+	}
+
+	/*print A - Z*/
+	while (ALP <= 'Z')
+	{
+		putchar(ALP);
+		++ALP;
+	}
+	putchar('\n');
+
+	return (0);
 }
