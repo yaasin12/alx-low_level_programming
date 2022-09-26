dec782d6299a3ea44e73f5e48f37e7d83898b3e9
yaasin12 <yassin.j@live.com>
2022-09-26 22:26:46 +0400

    fix

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
 /**
  * File: main.h
  * Auth: Yaasin
@@ -20,5 +17,3 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
-
-#endif
